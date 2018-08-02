8d42a87a24e17e1cf9b47203bc25b50c5f41896a
Timothy Farkas <tim@streamsimple.io>
2018-08-01 23:48:55 -0700

    - Added equals function for sets.

---
+++ src/collections/setutils.ts
@@ -0,0 +1,27 @@
+import {Collectable, JSet} from 'typescriptcollectionsframework';
+
+export class SetUtils {
+  /**
+   * This method requires both JSets to use the same Collectable implementation.
+   * @param {JSet<T>} thisSet
+   * @param {JSet<T>} thatSet
+   * @returns {boolean}
+   */
+  public static equals<T>(thisSet: JSet<T>, thatSet: JSet<T>) {
+    if (thisSet.size() !== thatSet.size()) {
+      return false;
+    }
+
+    let thisIterator = thisSet.iterator();
+
+    while (thisIterator.hasNext()) {
+      let thisElement = thisIterator.next();
+
+      if (!thatSet.contains(thisElement)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
