d77ed1a5954efb138bbb240d541a8dc6a8bf2cec
Timothy Farkas <tim@streamsimple.io>
2018-08-05 17:47:03 -0700

    - Added some map diff tests.

--- src/collections/maputils.test.ts
+++ src/collections/maputils.test.ts
@@ -1,7 +1,8 @@
 import * as chai from "chai";
 import {StringHashableImpl} from '../primitives/string';
-import {HashMap} from 'typescriptcollectionsframework';
+import {HashMap, ImmutableMap} from 'typescriptcollectionsframework';
 import {MapUtils} from './maputils';
+import {MapDiff, MapDiffCollectable, ValueDiff} from './mapdiff';
 
 const expect = chai.expect;
 
@@ -55,6 +56,40 @@ describe('MapUtils', () => {
       mapB.put('c', 'c1');
 
       expect(MapUtils.equals(mapA, mapB, StringHashableImpl.INSTANCE)).to.be.false;
+    });
+  });
+
+  describe('diff tests', () => {
+    it('should say empty maps are the same', () => {
+      let oldMap = new HashMap<string, string>(StringHashableImpl.INSTANCE);
+      let newMap = new HashMap<string, string>(StringHashableImpl.INSTANCE);
+
+      let mapDiff = MapUtils.diff(oldMap, newMap, StringHashableImpl.INSTANCE);
+
+      expect(mapDiff.isSame()).to.be.true;
+    });
+
+    it('should detect added key value pairs', () => {
+      let oldMap = new HashMap<string, string>(StringHashableImpl.INSTANCE);
+      oldMap.put('a', 'a1');
+      oldMap.put('b', 'b1');
+      oldMap.put('c', 'c1');
+
+      let newMap = new HashMap<string, string>(StringHashableImpl.INSTANCE);
+      newMap.put('a', 'a1');
+      newMap.put('b', 'b1');
+      newMap.put('c', 'c1');
+      newMap.put('d', 'd1');
+
+      let expectedAdded = new HashMap<string, string>();
+      expectedAdded.put('d', 'd1');
+
+      let expected = new MapDiff<string, string>(new HashMap(), expectedAdded, new HashMap());
+      let actual = MapUtils.diff(oldMap, newMap, StringHashableImpl.INSTANCE);
+
+      let mapDiffCollectable = new MapDiffCollectable(StringHashableImpl.INSTANCE);
+
+      expect(mapDiffCollectable.equals(expected, actual))
     });
   });
 });
